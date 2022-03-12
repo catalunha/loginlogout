93484fe9389d85c2cedd67f4389d1bc6c604d9c7
catalunha <catalunha.mj@gmail.com>
2022-03-12 20:34:52 -0300

    ok, so implementar firestore

--- lib/app/views/auth/login/login_page.dart
+++ lib/app/views/auth/login/login_page.dart
@@ -33,82 +33,88 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Login testes'),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text('Este app apenas testa login neste fluxo'),
-            const Text('1.1) Login email/senha - firebase Authentication'),
-            const Text('1.2) Login Google - firebase Authentication'),
-            const Text('2) Permissões acesso - firebase Firestore'),
-            TextButton(
-              child: Text('Cadastrar conta com Email/Senha'),
-              onPressed: () {
-                Get.toNamed(Routes.authRegister);
-              },
-            ),
-            Divider(),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const Text('Informe Email'),
-                  TextFormField(
-                    controller: _emailTec,
-                    validator: Validatorless.multiple(
-                      [
-                        Validatorless.email('Informe um email válido aqui.'),
-                        Validatorless.required('Campo obrigatorio'),
-                      ],
-                    ),
-                  ),
-                  const Text('Informe Senha'),
-                  TextFormField(
-                    controller: _passwordTec,
-                    validator: Validatorless.multiple(
-                      [
-                        Validatorless.min(6, 'Informe mais de 6 caracteres'),
-                        Validatorless.required('Campo obrigatorio'),
-                      ],
-                    ),
-                  ),
-
-                  // SizedBox(height: 10),
-                ],
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('Este app apenas testa login neste fluxo'),
+              const Text('1.1) Login email/senha - firebase Authentication'),
+              const Text('1.2) Login Google - firebase Authentication'),
+              const Text('2) Permissões acesso - firebase Firestore'),
+              Divider(),
+              TextButton(
+                child: Text('Cadastrar conta com Email/Senha'),
+                onPressed: () {
+                  Get.toNamed(Routes.authRegister);
+                },
               ),
-            ),
-            TextButton(
-              child: const Text('Login com Email/Senha'),
-              onPressed: () {
-                final formValid = _formKey.currentState?.validate() ?? false;
-                if (formValid) {
-                  widget._loginController
-                      .loginEmail(_emailTec.text, _passwordTec.text);
-                }
-              },
-            ),
-            TextButton(
-              child: Text('Esqueceu sua senha'),
-              onPressed: () {
-                if (_emailTec.text.isNotEmpty) {
-                  widget._loginController.forgotPassword(_emailTec.text);
-                } else {
-                  Get.snackbar(
-                    'Oops',
-                    'Digite email para prosseguir',
-                  );
-                }
-              },
-            ),
-            Divider(),
-            TextButton(
-              child: Text('Entre com o Google'),
-              onPressed: () {
-                widget._loginController.loginGoogle();
-              },
-            ),
-          ],
+              Divider(),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const Text('Informe Email'),
+                    TextFormField(
+                      controller: _emailTec,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.email('Informe um email válido aqui.'),
+                          Validatorless.required('Campo obrigatorio'),
+                        ],
+                      ),
+                    ),
+                    const Text('Informe Senha'),
+                    TextFormField(
+                      controller: _passwordTec,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.min(6, 'Informe mais de 6 caracteres'),
+                          Validatorless.required('Campo obrigatorio'),
+                        ],
+                      ),
+                    ),
+
+                    // SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              TextButton(
+                child: const Text('Login com Email/Senha'),
+                onPressed: () {
+                  final formValid = _formKey.currentState?.validate() ?? false;
+                  if (formValid) {
+                    widget._loginController
+                        .loginEmail(_emailTec.text, _passwordTec.text);
+                  }
+                },
+              ),
+              TextButton(
+                child: Text('Esqueceu sua senha'),
+                onPressed: () {
+                  if (_emailTec.text.isNotEmpty) {
+                    widget._loginController.forgotPassword(_emailTec.text);
+                  } else {
+                    Get.snackbar(
+                      'Oops',
+                      'Digite email para prosseguir',
+                    );
+                  }
+                },
+              ),
+              Divider(),
+              TextButton(
+                child: Text('Entre com o Google'),
+                onPressed: () {
+                  widget._loginController.loginGoogle();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
